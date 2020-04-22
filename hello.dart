76d8c84f88f740b6d310113870bc6560462b132b
Renar Freitas dos Santos <38799859+renarfreitas@users.noreply.github.com>
2020-04-22 17:11:05 -0300

    Commit inicial
    Iniciando os estudos com Flutter

---
+++ hello.dart
@@ -0,0 +1,16 @@
+main(){
+  var num1 = 2;
+  var num2 = 3;
+  var total_adic = num1 + num2;
+  var total_sub = num1 - num2;
+  var total_div = num1 / num2;
+  var total_rest = num1 % num2;
+  var total_mult = num1 * num2;
+
+	
+  print(total_adic);
+  print(total_sub);
+  print(total_div);
+  print(total_rest);
+  print(total_mult);
+}
